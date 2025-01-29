a18769853fd145a5ee7c549fb69fd29acb6d2144
Satunary <max.augsten@ah-solutions.at>
2025-01-29 08:35:28 +0100

    implemented use effect

--- Assets/scripts/core/DropHandler.cs
+++ Assets/scripts/core/DropHandler.cs
@@ -5,10 +5,18 @@ using UnityEngine.EventSystems;
 
 public class DropHandler : MonoBehaviour, IDropHandler
 {
-    
+    [SerializeField]
+    private GameObject abilityUtil;
+    private AbilityDecoder abilityDecoder;
+
+    private void Start()
+    {
+        abilityDecoder = abilityUtil.GetComponent<AbilityDecoder>();
+    }
+
     public void OnDrop(PointerEventData eventData)
     {
-        Debug.Log(transform.name);
+        
         if (transform.childCount == 0)
         {
             GameObject Dropped = eventData.pointerDrag;
@@ -18,4 +26,15 @@ public class DropHandler : MonoBehaviour, IDropHandler
             }
         }
     }
+
+    public void TriggerSPCEffect()
+    {
+        if(transform.childCount == 0)
+        {
+           string s = transform.GetChild(0).GetComponent<EffectDto>().effect;
+            abilityDecoder.Use(s);
+        }
+        
+    }
+
 }
