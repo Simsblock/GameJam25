387d353fbc454581d2f6addb3226b24a31556e77
Lojnik <loj210680@spengergasse.at>
2025-01-29 21:50:15 +0100

    HomeMenu

---
+++ Assets/scripts/Home/Core/HomeHandler.cs
@@ -0,0 +1,29 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HomeHandler : MonoBehaviour
+{
+    [SerializeField]
+    public TMP_Text Highscore_Text;
+
+    void Start()
+    {
+        Highscore_Text.text = $"Highscore\n{PlayerPrefs.GetInt("HighScore")}";
+    }
+
+    public void Play()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void Credits()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+}
